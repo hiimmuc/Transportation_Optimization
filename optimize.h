eec96e194a0910e6a081c66aed0e94344890d734
PhgNam Deng <NAM.DP181907@sis.hust.edu.vn>
2019-11-28 22:12:17 +0700

    team8 part

---
+++ optimize.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef optimize
+#define optimize
+
+int** hungarian_algo(int** input_mat, int num_of_rows, int num_of_cols);
+int** allocate(int number_of_row, int number_of_cols);
+void free_al(int** arr, int number_of_rows,int number_of_cols);
+void print_mat(int** arr, int num_of_rows, int num_of_cols);
+int** scan_mat(int** arr, int num_of_rows, int num_of_cols);
+
+#endif // !optimize.h
