0ed4b965436d501e4f95ac706e65ea45397e622b
hiimmuc <56968739+hiimmuc@users.noreply.github.com>
2019-12-10 20:25:47 +0700

    update

--- calculation_func.h
+++ calculation_func.h
@@ -1,10 +1,15 @@
 #pragma once
-/*nhom nguyen tuan trung*/
 
 #ifndef calculation_func
 #define calculation_func
+#ifdef __cplusplus
+extern "C" {
+#endif
+int** calculate_distances(int** init_distance, int** drivers, int** customers_initp, int** customers_finalp, int num_of_customers, int  num_of_drivers, int time);
+int** calculate_time_and_fee(int** input_mat, int** drivers, int** customers_initp, int** customers_finalp, int num_of_customers, int  num_of_drivers,int present_time);
 
-int** calculate_distances(int** drivers, int** customers_initp, int** customers_finalp, int num_of_customers, int  num_of_drivers);
-int** calculate_time_and_fee(int** input_mat, int** drivers, int** customers_initp, int** customers_finalp, int num_of_customers, int  num_of_drivers);
+#ifdef __cplusplus
+}
+#endif 
 
 #endif // !calculation
